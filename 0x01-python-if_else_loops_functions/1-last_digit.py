97dddb8e7bd8f284a209da8061433d1fe7f6511b
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-06 14:16:30 -0700

    python script that prints the last digit of the number stored in a variable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,14 +4,13 @@ number = random.randint(-10000, 10000)
 if (number > 0):
     last_number = number % 10
 else:
-    last_number = (number * -1) % 10
+    last_number = (number) % (-10)
 
 if (last_number > 5):
-    print("Last digit of {} is {} and is greater than 5"
-            .format(number, last_number))
+    print("Last digit of {} is {} and is greater \
+than 5".format(number, last_number))
 elif (last_number == 0):
-    print("Last digit of {} is {} and is 0"
-            .format(number, last_number))
+    print("Last digit of {} is {} and is 0".format(number, last_number))
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0"
-            .format(number, last_number))
+    print("Last digit of {} is {} and is less than \
+6 and not 0".format(number, last_number))
