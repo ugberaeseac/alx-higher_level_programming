4ef02948c856b55191602cc0de19352bc970d489
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-08 16:08:27 -0700

    Build your own calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+if (__name__ == "__main__"):
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    operators = {"+": add, "-": sub, "*":  mul, "/": div}
+    argc = len(argv) - 1
+    
+    if (argc != 3):
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+
+    if (argv[2] not in operators):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if (argv[2] == "+"):
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif (argv[2] == "-"):
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif (argv[2] == "*"):
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif (argv[2] == "/"):
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
