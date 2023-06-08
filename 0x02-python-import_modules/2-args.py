f5abadbac4d3636eaba2904535ec831d11f0aefb
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-08 11:24:04 -0700

    A program that prints the number of and the list of its arguments.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if (__name__ == "__main__"):
+    import sys
+    s = ""
+    n = len(sys.argv)
+    if (n < 2):
+        print("{} arguments.".format(n - 1))
+    elif (n == 2):
+        print("{} argument:".format(n - 1))
+    else:
+        print("{} arguments:".format(n - 1))
+    for i in range(1, n):
+        print("{}: {}".format(i, sys.argv[i]))
