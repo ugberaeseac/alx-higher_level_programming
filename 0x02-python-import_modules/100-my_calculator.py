7b0558bc6cd9a1498fdf3043cd98964b7c303d87
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-08 16:11:48 -0700

    fixed pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if (__name__ == "__main__"):
     from sys import argv
     operators = {"+": add, "-": sub, "*":  mul, "/": div}
     argc = len(argv) - 1
-    
+
     if (argc != 3):
         print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
