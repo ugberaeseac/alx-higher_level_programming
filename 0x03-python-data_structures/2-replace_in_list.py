4495f5d8b34401ab729b750d848e233c0bd14ee4
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-10 04:25:55 -0700

    fixed the correct number of elements in list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    count = len(my_list)
+    count = len(my_list) - 1
     if (idx < 0):
         return (my_list[:])
     elif (idx > count):
