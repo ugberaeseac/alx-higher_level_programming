665f5eacd296a6580c8cf8649f7857e257eaa49f
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-14 16:12:19 -0700

    changed sum to resultSUm because sum is a reserved keyword

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    sum = 0
-    toSet = set(my_list):
-        for i in toSet:
-            sum += i
-    return (sum)
+    resultSum = 0
+    toSet = set(my_list)
+    for i in toSet:
+        resultSum += i
+    return (resultSum)
