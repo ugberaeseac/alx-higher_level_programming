4dcc467a31708c9f31000fee27a14b910a7d3921
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-14 15:47:35 -0700

    debugging codeerror

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    if (my_list):
-        new_list = []
-        for i in my_list:
-            if i == search:
-                new_list.append(replace)
-            else:
-                new_list.append(i)
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
     return (new_list)
