b2655ca19178589990dacdbf01a69b14e4f7faa9
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-14 16:00:49 -0700

    used the sum function

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    toSet = set(my_list)
-    sum = 0
-    for i in toSet:
-        sum = sum(toSet)
+    toSet = set(my_lis):
+    sum = sum(toSet)
     return (sum)
