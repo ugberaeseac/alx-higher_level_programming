087417ab8656d794f2199ba5f2f1e3750599c776
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-26 13:22:44 -0700

    Fixed the count of the real number of element printed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
+            count = count + 1
     except (IndexError, ValueError):
         pass
     print()
-    return (x)
+    return (count)
