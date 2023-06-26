3db2006304a8fa76f980b8afb4744e5755f18b22
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-26 13:13:38 -0700

    Fixed indent error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
     except IndexError:
-            pass
+         pass
     print()
     return (x)
