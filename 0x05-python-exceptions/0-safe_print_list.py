f62f0b95807d3e8da72175fc39394c288547c36c
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-26 13:15:09 -0700

    implemented except for ValueError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
-    except IndexError:
+    except (IndexError, ValueError):
          pass
     print()
     return (x)
