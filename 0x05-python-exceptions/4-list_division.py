5ecb413b0ab2a4544f717e97c236cd50c7e66c35
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-26 16:04:48 -0700

    made file executable

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,16 +2,20 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
+    result = ""
     for i in range(list_length):
         result = 0
         try:
             result = my_list_1[i] / my_list_2[i]
+
         except(IndexError):
             print("out of range")
         except(TypeError):
             print("wrong type")
         except(ValueError, ZeroDivisionError):
             print("division by 0")
+
         finally:
             new_list.append(result)
+
     return (new_list)
