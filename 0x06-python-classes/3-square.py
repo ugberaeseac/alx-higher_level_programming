8e0dc0ed80043cfd6c356988fc700a7d0c08478c
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-06-27 15:18:34 -0700

    define a class square by private instance attribute and return the area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+A class Square that defines a square by
+private instance attribute
+"""
+
+
+class Square:
+    """
+    defines a class square
+
+    Attributes:
+        size: size of the square
+    """
+    def __init__(self, size=0):
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square
+
+
+        Returns:
+            The area of the square
+        """
+        return (self.__size ** 2)
