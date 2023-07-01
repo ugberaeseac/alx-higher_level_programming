38044487f9b3efb948fb001ed66c4e29324bdfca
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-07-01 16:38:35 -0700

    A function that adds two integers and tested with doctest

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: 0-add_integer
+Takes two arguments, casts them to integers and computes the sum
+Returns an integer: the sum of 'a' and 'b'
+"""
+
+
+def add_integer(a, b=98):
+    """ 
+    Adds two integers and return an int
+    """
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    elif type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+    else:
+        return (int(a) + int(b))
