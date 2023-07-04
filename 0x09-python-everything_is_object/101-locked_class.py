bac61f8af244b8d8e8acb5618142901f77de9a3e
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-07-04 14:32:53 -0700

    debug

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
-
-"""
-Module: 101-locked_class
-class with no object attribute that prevents user
-from dynamically creating new instance attributes except first_name
-"""
-
-
 class LockedClass:
     """
-    prevent dynamically created instances
-    except 'first_name'
+    prevent dynamically created instances except 'first_name'
     """
-
-    __slots__ = "first_name"
+    __slots__ = ["first_name"]
