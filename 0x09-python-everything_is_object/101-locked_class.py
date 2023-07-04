70977793b09d95d560c0f0e78364d626d9af75d7
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-07-04 14:47:24 -0700

    debug

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
 """
-Defines class with no class or object attribute
-Control dynamically created instance attributes
-https://www.python-course.eu/python3_slots.php
+Module: 101-locked_class
+class with no class or object attribute
+that prevents the user from dynamically creating new instance attributes
 """
 
 
 class LockedClass():
     """
-    prevent user from creating new instance attribute dynamically
-    unless attribute is "first_name"
-
-    >>> a = LockedClass()
-    >>> a.first_name = 'Melissa'
-    >>> a.first_name
-    'Melissa'
-
-    >>> a.last_name = 'Ng'
-    Traceback (most recent call last):
-    ...
-    AttributeError: 'LockedClass' object has no attribute 'last_name'
+    prevent dynamically created instances 
+    except instance attribute is 'first_name'
     """
 
     __slots__ = "first_name"
