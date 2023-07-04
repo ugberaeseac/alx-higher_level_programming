c0f8d680ed40fea3643970b401ec4e4027b2212d
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-07-04 14:06:31 -0700

    class that prevents the creation of a dynamic new instance attribute

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Module: 101-locked_class
+class with no object attribute that prevents user
+from dynamically creating new instance attributes except first_name
+"""
+
+
+def LockedClass:
+    """
+    prevent dynamically created instances except 'first_name'
+    """
+    __slots__ = ["first_name"]
