c18e97d4ebe7e91b9471fd983c6367e093484ff1
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-07-05 12:20:25 -0700

    method/function that divides all elements of matrix and returns a new matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""
+Module: 2-matrix_divided
+Method that divides all elements of a matrix by a divisor
+and returns a new list of matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    divides elements of matrix by div
+    and returns a new matrix
+    """
+    new_matrix = []
+    
+    if type(matrix) is not list:
+        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    elif len(matrix) == 0 or len(matrix[0]) == 0:
+        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+
+    for rows in matrix:
+        if type(rows) is not list:
+            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+        if len(rows) != len(matrix[0]):
+            raise TypeError('Each row of the matrix must have the same size')
+        
+        new_list = []
+        for cols in rows:
+            if type(cols) not in [int, float]:
+                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            if type(div) not in [int, float]:
+                raise TypeError('div must be a number')
+            new_list.append(round(cols/div, 2))
+        new_matrix.append(new_list)
+
+    return new_matrix
