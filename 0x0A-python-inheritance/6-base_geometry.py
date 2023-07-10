3627449dc984bdab5651da656e26d5ab851c78d6
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-07-10 14:59:22 -0700

    raise an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Module: 6-base_geometry
+class that raises an exeption
+"""
+
+
+class BaseGeometry:
+    """
+    raise exception
+    """
+    def area(self):
+        """ raise an exception"""
+        raise Exception('area() is not implemented')
