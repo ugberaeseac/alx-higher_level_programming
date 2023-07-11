fc971d15f0ef7a152611791f34892956ad8af12e
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-07-11 16:42:30 -0700

    script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -14,4 +14,4 @@ def load_from_json_file(filename):
     create object from JSON file
     """
     with open(filename, mode='r', encoding='utf-8') as myFile:
-        return(json.loads(myFile.read()))
+        return (json.loads(myFile.read()))
