a7781a190e78471a85e480841821d1bceb810990
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-07-10 17:24:33 -0700

    added missing documentation

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -29,7 +29,9 @@ class Rectangle(BaseGeometry):
     and initializaion of attributes
     """
     def __init__(self, width, height):
-        """ validate and initialize """
+        """
+        validate and initialize
+        """
         super().integer_validator("width", width)
         self.__width = width
         super().integer_validator("height", height)
