429ecebefaaf1087b376d0e1894f4f48fd3ec512
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-07-10 18:22:46 -0700

    Class inherits from class int and have operators inverted

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+Module: 100-my_int
+Class MyInt that inherits from int
+and have operators inverted
+"""
+
+
+class MyInt(int):
+    """
+    invert operators
+    """
+    def __init__(self, num):
+        """ initialize attribute"""
+        self.__num = num
+
+    def __str__(self):
+        """
+        print representation/description of attribute
+        """
+        return (str(self.__num))
+
+    def __eq__(self, other):
+        """ compare for equality """
+        return(self.__num != other)
+
+    def __ne__(self, other):
+        """ compare for non-equality """
+        return (self.__num == other)
