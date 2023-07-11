94a77dff30f05ad08a330488664582861321f065
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-07-10 17:20:29 -0700

    Class Rectangle inherits from class BaseGeometry, validate value and initialize

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+Module: 8-base_geometry
+Rectangle class that inherits from BaseGeometry class
+and validates an integer value for height and width
+"""
+
+
+class BaseGeometry:
+    """
+    raise exception
+    """
+    def area(self):
+        """ raise an exception"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """ validates value """
+        if type(value) not in [int]:
+            raise TypeError('{:s} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{:s} must be greater than 0'.format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    integer validation of width and height
+    and initializaion of attributes
+    """
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
