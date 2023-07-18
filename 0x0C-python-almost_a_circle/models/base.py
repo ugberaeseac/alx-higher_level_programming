f402ee2318b4e907e7dc2e5a6f2721f1fa58269e
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-07-18 03:11:09 -0700

    fixed empty list of instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -54,12 +54,11 @@ class Base:
         writes the JSON string representation of a list of Base instance
         """
         objs_list = []
-        filename = cls.__name__ + ".json"
 
         if list_objs is not None or len(list_objs) == 0:
             for objs in list_objs:
                 objs_list.append(cls.to_dictionary(objs))
-
+        filename = cls.__name__ + "json"
         with open(filename, mode='w' encoding='utf-8') as myFile:
             myFile.write(cls.to_json_string(objs_list))
 
