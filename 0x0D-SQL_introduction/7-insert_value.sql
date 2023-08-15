542b34967f746846b82fe1b781f5f064fcd1a609
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-08-15 14:24:26 -0700

    insert into table first_table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 --  script that inserts a new row in the table first_table
 -- on the MySQL server
-INSERT INTO first_name (`id`, `name`) VALUES (89, 'Best School');
+INSERT INTO first_table (`id`, `name`) VALUES (89, 'Best School');
