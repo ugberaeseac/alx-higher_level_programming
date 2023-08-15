849937223a5b2966b87221fb5b9b3f08e15a186c
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-08-15 15:31:46 -0700

    script that lists specific columns of the table in descending order of score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- script that lists all records of the table second_table
+-- Results set display both the score and the name and
+-- should be ordered by the score
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
