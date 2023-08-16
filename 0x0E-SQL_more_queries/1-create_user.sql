eab7e72b70b6210f77a22ab7fe54cc5a99256d9d
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-08-16 13:59:17 -0700

    script that creates the MySQL server user and grants all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,5 +4,5 @@
 -- grant all privileges to user_0d_1
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
