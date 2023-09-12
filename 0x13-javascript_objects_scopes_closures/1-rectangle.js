bf4504954e26d5dd233ba3d81c816eb62ad4bcb4
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-09-12 15:32:46 -0700

    Rectangle class with constructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+class Rectangle {
+  /* class that defines a Rectangle object */
+  constructor (w, h) {
+  /* constructor */
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
