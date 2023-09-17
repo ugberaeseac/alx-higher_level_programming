ea44f9d4b0a82d77457e83b78a65f936546e52f0
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-09-17 16:15:04 -0700

    A script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 
 """
-list the first State objects from the database hbtn_0e_6_usa
+lists all State objects that contain the letter a from the database
 takes 3 arguments: mysql username, mysql password and database name
 imports State and Base from model_state
 connects to a MySQL server running on localhost at port 3306
 results sorted in ascending order by states.id
-print Nothing if the table is empty
 """
 
 
-from sys import argv
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sys import argv
 
 
 if __name__ == '__main__':
@@ -26,8 +25,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    queryData = session.query(State).order_by(State.id).first()
-    if queryData:
-        print("{}: {}".format(queryData.id, queryData.name))
-    else:
-        print("Nothing")
+    queryData = session.query(State).filter(
+            State.name.like('%a%')).order_by(State.id).all()
+    for data in queryData:
+        print("{}: {}".format(data.id, data.name))
