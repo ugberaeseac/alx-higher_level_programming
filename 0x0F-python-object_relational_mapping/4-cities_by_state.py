e6de3906700cb585dc633b70895218f604aef180
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-09-17 18:53:45 -0700

    fixed bug

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -21,8 +21,8 @@ if __name__ == '__main__':
     cursor = dBase.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name\
                     FROM cities\
-                    LEFT JOIN states\
-                    ON cities.id = states.id\
+                    JOIN states\
+                    ON cities.state_id = states.id\
                     ORDER BY cities.id ASC;")
     queryRows = cursor.fetchall()
 
