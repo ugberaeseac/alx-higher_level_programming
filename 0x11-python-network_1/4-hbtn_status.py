ca27f1affa7724a7a435761569cbce1b1e2325af
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-10-02 18:21:13 -0700

    debug

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,7 +10,8 @@ import requests
 
 
 if __name__ == "__main__":
-    httpResponse = requests.get('https://intranet.hbtn.io/status')
+    url = "https://intranet.hbtn.io/status"
+    httpResponse = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(httpResponse.text)))
     print("\t- content: {}".format(httpResponse.text))
