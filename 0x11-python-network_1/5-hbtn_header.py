86c6e34b15d5d3d41d3d0f5c8586663208d68502
Ugberaese Charles A <ugberaeseac@gmail.com>
2023-12-22 14:37:05 -0800

    Python script that takes in a URL, sends a request to the URL and displays the value of the variable

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
+"""
+given the URL and email, sends a request to the URL and displays the value of the variable 
+"""
+import requests
+from sys import argv
+
+
 if __name__ == "__main__":
-    r = requests.get(argv[1])
-    print(r.headers.get('X-Request-Id'))
+    response = requests.get(argv[1])
+    print(response.headers.get('X-Request-Id'))
