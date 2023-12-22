e6225baba5b136a369a79e7e4515f05f70eba9c5
Ugberaese Charles A <ugberaeseac@gmail.com>
2023-12-22 14:31:15 -0800

    Python script that takes in a URL and an email address

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+given the URL and email, send POST request to URL 
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    mail = {'email': argv[2]}
+    response = requests.post(url, data=mail)
+    print(response.text)
